d640e1ffebd6aae3275f7bc8ae78f4255f38e5d0
Justin <justinereifanwijaya@gmail.com>
2025-04-09 14:44:26 +0700

    replace all files (delete)

--- resources/js/Components/PrimaryButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button
-        class="inline-flex items-center px-3 py-2 space-x-2 text-white transition-all duration-200 rounded-lg shadow-lg sm:px-4 bg-primary hover:bg-primary-hover hover:shadow-glow-primary"
-    >
-        <slot />
-    </button>
-</template>
