70254bb56ef6fc62c4c315162e8c51c3a77c5c4a
Justin <justinereifanwijaya@gmail.com>
2025-07-24 23:13:01 +0700

    creating paket layanan features

---
+++ app/Http/Controllers/PaketLayananController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaketLayanan;
+use Illuminate\Http\Request;
+
+class PaketLayananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaketLayanan $paketLayanan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PaketLayanan $paketLayanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PaketLayanan $paketLayanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PaketLayanan $paketLayanan)
+    {
+        //
+    }
+}
